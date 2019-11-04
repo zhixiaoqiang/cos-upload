3a4db011128f522a11e3944f663bc4b6fcf2e47d
誌小強 <oncwnuP0rnVMPHgiWqo-39AWDsN0@git.weixin.qq.com>
2019-11-04 13:33:12 +0800

    feat: add publish scripts

---
+++ scripts/prod.js
@@ -0,0 +1,21 @@
+const chalk = require('chalk')
+const execa = require('execa')
+const bumpVersion = require('./bump-version')
+
+;(async () => {
+  console.log('publish start...')
+  const isConfirm = await bumpVersion()
+  console.warn(isConfirm)
+  const stdio = { stdio: 'inherit' }
+  if (isConfirm) {
+    await execa('yarn', ['release'], stdio)
+    await execa('yarn', ['publish'], stdio)
+
+    console.log(chalk.green('publish successful.'))
+  }
+})().catch((err) => {
+  console.log(err)
+  err.stderr && console.error(err.stderr)
+  err.stdout && console.error(err.stdout)
+  process.exit(1)
+})
