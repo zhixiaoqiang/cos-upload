2bee7625710b3a6992079e4ddf3a5718fed7796e
誌小強 <oncwnuP0rnVMPHgiWqo-39AWDsN0@git.weixin.qq.com>
2019-11-05 15:46:03 +0800

    feat: add rollup package

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "modules": false,
+        "useBuiltIns": "usage",
+        "corejs": "3",
+        "targets": {
+          "ie": 10
+        }
+      }
+    ]
+  ],
+  "plugins": [
+      // 解决多个地方使用相同代码导致打包重复的问题
+      ["@babel/plugin-transform-runtime"]
+  ],
+  "ignore": [
+      "node_modules/**"
+    ]
+}
---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import json from 'rollup-plugin-json'; 
+import resolve from 'rollup-plugin-node-resolve'; // 帮助 Rollup 查找外部模块，然后导入
+import common from 'rollup-plugin-commonjs' // 将CommonJS模块转换为 ES2015 供 Rollup 处理
+import babel from 'rollup-plugin-babel';
+import builtins from 'rollup-plugin-node-builtins'
+import { terser } from 'rollup-plugin-terser';
+
+
+const outputCommonOptions = {
+  globals: {
+    axios: 'Api',
+    dayjs: 'dayJs',
+  }
+}
+
+export default {
+  input: 'index.js',
+  external: [ 'axios', 'dayjs'],
+  output: [
+    {
+      file: 'dist/cjs/index.js',
+      format: 'cjs',
+      ...outputCommonOptions,
+    },
+    {
+      file: 'dist/esm/index.js',
+      format: 'esm',
+      ...outputCommonOptions,
+    },
+    {
+      file: 'dist/umd/index.js',
+      name: 'index',
+      format: 'umd',
+      ...outputCommonOptions,
+    }
+  ],
+  plugins: [
+    builtins(),
+    resolve({
+      preferBuiltins:true,
+    }), 
+    common(),
+    
+    json(),
+    babel({
+      exclude: 'node_modules/**', // 防止打包node_modules下的文件
+      runtimeHelpers: true, // 使plugin-transform-runtime生效
+    }),
+    terser(),
+  ]
+};
